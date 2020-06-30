c99f8b8d95051482b710b5ecb1007465a55bc6ce
rosudel <67065387+rosudel@users.noreply.github.com>
2020-06-30 17:25:51 +0600

    Update MaxEvenSub-arrayBydp.cpp

--- MaxEvenSub-arrayBydp.cpp
+++ MaxEvenSub-arrayBydp.cpp
@@ -1,4 +1,8 @@
 
+/* find maximum sum subarray of even length using dp whose recurrence is simply
+
+dp[i] = max({0,arr[i]+arr[i+1],arr[i]+arr[i+1]+dp[i+2]) .*/
+
 
 ll maxSubArraySum(ll a[], ll size)
 {
