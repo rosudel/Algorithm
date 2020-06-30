80f5c1b4ae6b28a303943ea4061455a845a9cbea
rosudel <67065387+rosudel@users.noreply.github.com>
2020-06-30 17:02:46 +0600

    It returns the maximum subarray sum of even length
    it is dp approach.It returns the maximum subarray sum of even length

---
+++ MaxEvenSub-arrayBydp.cpp
@@ -0,0 +1,15 @@
+
+
+ll maxSubArraySum(ll a[], ll size)
+{
+    ll dp[size];
+    dp[0] = 0;
+    dp[1] = a[0]+a[1];
+    ll maxi = max(0ll,dp[1]);
+    for(ll i = 2; i < size; i++){
+        dp[i] = max((a[i]+a[i-1]),(a[i]+a[i-1]+dp[i-2]));
+        maxi = max(maxi,dp[i]);
+    }
+    return maxi;
+}
+
